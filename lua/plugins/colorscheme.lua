2814c8f21b41fcd80eb854bfec0cce30cdfb9e18
Mateus Oliveira <amateussteam@gmail.com>
2025-10-08 22:32:36 -0300

    renamed modules

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,14 @@
 return {
   {
     "catppuccin/nvim",
-    lazy = false,
+    lazy = true,
     name = "catppuccin",
     -- you can do it like this with a config function
     config = function()
       require("catppuccin").setup({
-        -- configurations
+        flavour = "mocha",
       })
     end,
-    -- or just use opts table
-    opts = {
-      -- configurations
-    },
   },
   {
     "LazyVim/LazyVim",
